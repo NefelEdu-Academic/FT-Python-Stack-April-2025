ff2d928515575f019dfa5897ab2871df989d985f
waeeelhajji <waelhajji.contact@gmail.com>
2025-04-29 16:25:45 +0100

    Week2Day2

---
+++ Week2Day2/W2D2/Algo/w2d2.js
@@ -0,0 +1,49 @@
+/* 
+  Given an array of strings
+  return the number of times each string occurs (a frequency / hash table)
+  
+*/
+
+
+var arr1 = ["a", "a", "a"];
+var expected1 = {
+  a: 3,
+};
+
+var arr2 = ["a", "b", "a", "c", "B", "c", "c", "d"];
+var expected2 = {
+  a: 2,
+  b: 1,
+  c: 3,
+  B: 1,
+  d: 1,
+};
+
+var arr3 = [];
+var expected3 = {};
+expected3['a']=1
+expected3['a']+=1
+const username = "Joe"
+//! username = "Jane"
+const pi=3.14
+//! pi+=1
+let result = true
+result = false
+console.log(expected3)
+function makeFrequencyTable(arr) {
+    //   Your Code Here
+    var y;
+    // clg(arr2)
+    var x = 0;
+    // 
+    // clg(y) --> undefined
+    // clg(z)--> error --> z is not defined
+    var y =1;
+    let z;
+}
+// clg(z)
+// clg(y)
+
+makeFrequencyTable(arr1);
+makeFrequencyTable(arr2);
+makeFrequencyTable(arr3);
